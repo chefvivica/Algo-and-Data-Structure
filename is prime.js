6c3011ed43b5a343b777c53a31ba3bfd0d20c89a
chefvivica <vivicacorn@gmail.com>
2021-08-21 22:06:05 -0400

    add some easy problems

---
+++ is prime.js
@@ -0,0 +1,20 @@
+/**
+ Write a function, isPrime, that takes in a number as an argument. The function should return a boolean indicating whether or not the given number is prime.
+
+A prime number is a number that is only divisible two distinct numbers: 1 and itself.
+
+For example, 7 is a prime because it is only divisible by 1 and 7. For example, 6 is not a prime because it is divisible by 1, 2, 3, and 6.
+
+You can assume that the input number is a positive integer.
+ */
+
+const isPrime = (n) => {
+  
+    if( n < 2) return false;
+    
+    //time complexity O(square_root(n)) // space o(1)
+    for( let i = 2; i <= Math.sqrt(n); i +=1){
+      if( n % i === 0) return false;
+    }
+    return true;
+  };
---
+++ max value.js
@@ -0,0 +1,23 @@
+/**
+max value
+Write a function, maxValue, that takes in array of numbers as an argument. The function should return the largest number in the array.
+
+Solve this without using any built-in array methods.
+
+You can assume that the array is non-empty.
+**/
+
+const maxValue = (nums) => {
+  let max = nums[0];
+  for(let i = 1; i < nums.length; i++){
+    if(nums[i] >= max){
+      max = nums[i]
+    }
+  }
+  
+  return max;
+};
+
+
+
+
