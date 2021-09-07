0c3f69813df7cded662684a6227162506dd251a5
chefvivica <vivicacorn@gmail.com>
2021-09-06 22:53:24 -0400

    merge lists

---
+++ merge list.js
@@ -0,0 +1,34 @@
+const mergeLists = (head1, head2) => {
+    let head = null;
+    let current1 = null;
+    let current2 = null;
+    if(head1.val < head2.val){
+      head = head1
+      current1 = head1.next
+      current2 = head2
+    }else{
+      head = head2
+      current1 = head1
+      current2 = head2.next;
+    }
+    
+    let tail = head
+    while(current1 !== null && current2 !== null){
+      if(current1.val < current2.val){
+        tail.next = current1;
+        current1 = current1.next  
+      }else{
+        tail.next = current2;
+        current2 = current2.next  
+      }
+      tail = tail.next
+    }
+    
+    if(current1 !== null) tail.next = current1;
+    if(current2 !== null) tail.next = current2;
+    
+    return head;
+    
+  };
+
+  // time O(min(n,m)) space o(1)
